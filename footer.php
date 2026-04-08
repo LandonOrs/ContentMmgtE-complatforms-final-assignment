7b6114413a6f98866e27af2f8bc88d38b168037a
LandonOrs <fr00gy001@gmail.com>
2026-04-07 23:16:01 -0400

    added footer template

---
+++ footer.php
@@ -0,0 +1,10 @@
+
+<footer>
+    <div class="container">
+        <p>&copy; <?php echo date('Y'); ?> Landon Fitness. All rights reserved.</p>
+        <p>Built for Final Project COMP2109</p>
+    </div>
+    <?php wp_footer(); ?>
+</footer>
+</body>
+</html>
